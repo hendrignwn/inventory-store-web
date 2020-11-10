ca5679e3bd9f7a862206865a15815c8d0937702e
Hendri <hendri.gnw@gmail.com>
2020-11-10 11:50:22 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                        <select class="select2">
+                                <option id="Ok">ASd</option>
+                        </select>
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @section('plugins.Datatables', true)
+</div>
+@endsection
